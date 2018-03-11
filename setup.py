8c058b2a50e6cf41028abaedd70cd413584cb59d
Dipesh Acharya <xtranophilist@gmail.com>
2018-03-11 15:50:43 +0545

    Create django_hbl python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django_hbl',
+      version='0.1',
+      description='Himalayan Bank Payment Gateway with django-payments',
+      url='http://github.com/xtranophilist/django_hbl',
+      author='Dipesh Acharya',
+      author_email='dipesh@awecode.com',
+      license='MIT',
+      packages=['django_hbl'],
+      zip_safe=False)
