a2892a83d23de990bdb9e874287842847c626d82
Dipesh Acharya <xtranophilist@gmail.com>
2018-03-11 19:45:49 +0545

    Update setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from setuptools import setup
 
-setup(name='django_hbl',
-      version='0.1',
+setup(name='django-payments-hbl',
+      version='0.1.1',
       description='Himalayan Bank Payment Gateway with django-payments',
-      url='http://github.com/xtranophilist/django_hbl',
+      url='http://github.com/xtranophilist/django-payments-hbl',
       author='Dipesh Acharya',
       author_email='dipesh@awecode.com',
-      license='MIT',
-      packages=['django_hbl'],
-      zip_safe=False)
+      maintainer='Awecode',
+      license='BSD License',
+      packages=['django_payments_hbl'],
+      long_description=open('README.md').read(),
+      include_package_data=True,
+      zip_safe=False,
+      keywords='froala,django,admin,wysiwyg,editor,text,html,editor,rich, web',
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      )
